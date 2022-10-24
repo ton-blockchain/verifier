257c49d9b4a703836c261929847e3d99746341dc
Shahar Yakir <shahar.yakir@gmail.com>
2022-10-24 12:17:30 +0300

    basic routing

--- src/components/AddressInput.tsx
+++ src/components/AddressInput.tsx
@@ -1,7 +1,19 @@
 import "./AddressInput.css";
+import { useNavigate } from "react-router-dom";
 
 function AddressInput() {
-  return <input className="AddressInput" />;
+  const navigate = useNavigate();
+  return (
+    <input
+      className="AddressInput"
+      type="search"
+      onKeyDown={(e: any) => {
+        if (e.keyCode === 13) {
+          navigate(`/${e.target.value}`);
+        }
+      }}
+    />
+  );
 }
 
 export default AddressInput;
