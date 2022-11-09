625a893f0ced7dc94c08d5ee711b813d097b46d7
Shahar Yakir <shahar.yakir@gmail.com>
2022-11-09 13:28:47 +0200

    with styling

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,7 +5,7 @@ import WalletConnect from "./WalletConnect";
 function TopBar() {
   return (
     <div className="TopBar">
-      <img src={icon} alt='Logo icon' />
+      <img src={icon} alt="Logo icon" />
       <h3>TON VERIFY</h3>
       <div className="TopBar-Right">
         <WalletConnect />
