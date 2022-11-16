d562340707b43e7007ed1c627cc82477575099ef
Shahar Yakir <shahar.yakir@gmail.com>
2022-11-16 13:06:38 +0200

    fixenv

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
-  build: {
-    minify: false, // TODO temporary
-  },
 });
