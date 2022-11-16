7baad55b950da1acd93c3ee717c35139cf73e72c
Shahar Yakir <shahar.yakir@gmail.com>
2022-11-16 12:08:03 +0200

    temporarily removing minify

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
+  build: {
+    minify: false, // TODO temporary
+  },
 });
