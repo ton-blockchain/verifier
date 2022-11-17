1a23f7dd56ffa0cb1f9c40ec5a2eb94077082a4b
antongotech <105588893+antongotech@users.noreply.github.com>
2022-11-17 10:02:27 +0200

    icons added, notification box modified

--- src/components/CompilationNotification.tsx
+++ src/components/CompilationNotification.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box, styled } from "@mui/system";
 
 enum NotificationType {
   ERROR = "Error",
@@ -7,10 +8,51 @@ enum NotificationType {
   NOTIFICATION = "Notification",
 }
 
-interface CompilationNotificationProps {
-  type: NotificationType;
+interface NotificationBoxProps {
+  borderColor?: string;
+  backgroundColor?: string;
 }
 
-export function CompilationNotification() {
-  return <div></div>;
+const NotificationBox = styled(Box)(({ theme }) => (props: NotificationBoxProps) => ({
+  background: props.backgroundColor || "",
+  border: `1px solid ${props.borderColor || "#D8D8D8"}`,
+  borderRadius: 12,
+}));
+
+interface CompilationNotificationProps {
+  type: NotificationType;
+  title: React.ReactNode;
+  notificationBody: React.ReactNode;
+}
+
+export function CompilationNotification({
+  title,
+  type,
+  notificationBody,
+}: CompilationNotificationProps) {
+  let borderColor;
+  let backgroundColor;
+
+  switch (type) {
+    case NotificationType.NOTIFICATION:
+      backgroundColor = "rgba(216, 216, 216, 0.2);";
+      break;
+    case NotificationType.ERROR:
+      borderColor = "rgba(252, 86, 86, 0.42);";
+      backgroundColor = "rgba(252, 86, 86, 0.08);";
+      break;
+    case NotificationType.HINT:
+      backgroundColor = "rgba(94, 117, 232, 0.1);";
+      break;
+    case NotificationType.SUCCESS:
+      backgroundColor = "#D6FFCE";
+      break;
+  }
+
+  return (
+    <NotificationBox borderColor={borderColor} backgroundColor={backgroundColor}>
+      {title}
+      {notificationBody}
+    </NotificationBox>
+  );
 }
