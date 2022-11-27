9a2541af10e6d6a21281456bcde952f6e715f097
antongotech <105588893+antongotech@users.noreply.github.com>
2022-11-27 13:54:35 +0200

    directory trimmed (#54)
    * directory trimmed
    
    * trimming fn fixed

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -1,7 +1,15 @@
-export const makeElipsisAddress = (address?: string | null, padding?: number): string => {
+const makeElipsisAddress = (address?: string | null, padding?: number): string => {
   const paddingValue = padding || 10;
   if (!address) return "";
   const firstPart = address.substring(0, paddingValue);
   const secondPart = address.substring(address.length - paddingValue);
   return `${firstPart}...${secondPart}`;
 };
+
+const trimDirectory = (str: string): string =>
+  str
+    .replace(/\/+/g, "/")
+    .replace(/^\/[^\/]/, "")
+    .replace(/\/$/, "");
+
+export { makeElipsisAddress, trimDirectory };
