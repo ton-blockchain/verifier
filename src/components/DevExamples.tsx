1254d9f3db9f3e797397edc9629e3510e92a4b1b
Shahar Yakir <shahar.yakir@gmail.com>
2022-12-08 16:32:44 +0200

    in prog

--- src/components/DevExamples.tsx
+++ src/components/DevExamples.tsx
@@ -2,27 +2,46 @@ import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { download } from "../utils/jsonUtils";
 import { AppButton } from "./AppButton";
-const examples_not_verified = [
-  ["wallet v1r1", "EQAAQ-CfIZkUjmZ6ES9D_keK2yHz10U1ba49K0S86Whva74Z"],
-  ["wallet v1r2", "EQAAVd4c_2pMb4Bp8BxumyV8jutdwJ9R-q0dBqQj7tj_W8SX"],
-  ["wallet v1r3", "EQAAEgdraul87g9zvm5Lxtd9FNoebifojeyT90uG6zrWBvRh"],
-  ["wallet v2r1", "EQAAC2tOLQxG4KuFcS_pb2Rta1MDdgx8wAtZnGf5bIEIMLft"],
-  ["wallet v2r2", "EQAAnU-irJsuuljRAWBRUhdvFB-rvGRHbdQSWXPSQYND6MVb"],
-  ["wallet v3r1", "EQAY_2_A88HD43S96hbVGbCLB21e6_k1nbaqICwS3ZCrMBaZ"],
-  ["wallet v3r2", "EQALgHQ-KpmkwftbsdeZdA4DvVDCYkKvria9llb7_RMeZj_8"],
-  ["system", "Ef8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAU"],
-];
+const examples_not_verified = [["system", "Ef8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAU"]];
 
 const examples = [
-  ["restricted (wallet?)", "Ef90OvMkpTFDt1uQ5OPa4cRqDtKvc6CCt3pMVidDBX7waNnO"],
-  ["dns-root", "Ef_lZ1T4NCb2mwkme9h2rJfESCE0W34ma9lWp7-_uY3zXDvq"],
-  ["wallet-v4", "EQDerEPTIh0O8lBdjWc6aLaJs5HYqlfBN2Ruj1lJQH_6vcaZ"],
-  ["dns-collection", "EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz"],
-  ["dns-item", "EQAGSjhQajnMSne9c9hGnKdMKmohX2-MkZuOkk7TmwQKwFOU"],
-  ["counter", "EQC-QTihJV_B4f8M2nynateMLynaRT_uwNYnnuyy87kam-G7"],
-  ["jetton-minter-discoverable", "EQD-LkpmPTHhPW68cNfc7B83NcfE9JyGegXzAT8LetpQSRSm"],
-  ["jetton-minter", "EQBb4JNqn4Z6U6-nf0cSLnOJo2dxj1QRuGoq-y6Hod72jPbl"],
-  ["jetton-wallet", "EQAhuLHxOcrBwwMHKDnCUMYefuHwJ2iTOFKHWYQlDD-dgb__"],
+  [
+    "Wallets - FIFT",
+    [
+      ["wallet v1r1", "EQAAQ-CfIZkUjmZ6ES9D_keK2yHz10U1ba49K0S86Whva74Z"],
+      ["wallet v1r2", "EQAAVd4c_2pMb4Bp8BxumyV8jutdwJ9R-q0dBqQj7tj_W8SX"],
+      ["wallet v1r3", "EQAAEgdraul87g9zvm5Lxtd9FNoebifojeyT90uG6zrWBvRh"],
+      ["wallet v2r1", "EQAAC2tOLQxG4KuFcS_pb2Rta1MDdgx8wAtZnGf5bIEIMLft"],
+      ["wallet v2r2", "EQAAnU-irJsuuljRAWBRUhdvFB-rvGRHbdQSWXPSQYND6MVb"],
+      ["wallet v3r1", "EQAY_2_A88HD43S96hbVGbCLB21e6_k1nbaqICwS3ZCrMBaZ"],
+      ["wallet v3r2", "EQALgHQ-KpmkwftbsdeZdA4DvVDCYkKvria9llb7_RMeZj_8"],
+    ],
+  ],
+  [
+    "DNS",
+    [
+      ["dns-root", "Ef_lZ1T4NCb2mwkme9h2rJfESCE0W34ma9lWp7-_uY3zXDvq"],
+      ["dns-collection", "EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz"],
+      ["dns-item", "EQAGSjhQajnMSne9c9hGnKdMKmohX2-MkZuOkk7TmwQKwFOU"],
+    ],
+  ],
+  [
+    "Jettons",
+    [
+      ["jetton-minter-discoverable", "EQD-LkpmPTHhPW68cNfc7B83NcfE9JyGegXzAT8LetpQSRSm"],
+      ["jetton-minter", "EQBb4JNqn4Z6U6-nf0cSLnOJo2dxj1QRuGoq-y6Hod72jPbl"],
+      ["jetton-wallet", "EQAhuLHxOcrBwwMHKDnCUMYefuHwJ2iTOFKHWYQlDD-dgb__"],
+    ],
+  ],
+  [
+    "Other",
+    [
+      ["restricted (wallet?)", "Ef90OvMkpTFDt1uQ5OPa4cRqDtKvc6CCt3pMVidDBX7waNnO"],
+      ["counter", "EQC-QTihJV_B4f8M2nynateMLynaRT_uwNYnnuyy87kam-G7"],
+    ],
+  ],
+  ["Wallets", [["wallet-v4", "EQDerEPTIh0O8lBdjWc6aLaJs5HYqlfBN2Ruj1lJQH_6vcaZ"]]],
+
   ["single-nominator", "Ef_BLbagjGnqZEkpURP96guu7M9aICAYe5hKB_P5Ng5Gju5Y"],
   ["sources-registry", "EQD-BJSVUJviud_Qv7Ymfd3qzXdrmV525e3YDzWQoHIAiInL"],
   ["nominator-pool", "Ef8iu8EiNOP2MczVvHseFi-CrGO1C4v6MkSSOgVZcESNGfT7"],
