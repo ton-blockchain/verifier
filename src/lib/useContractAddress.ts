5e181c2880b984534213c945b3a1a8627cf772e9
antongotech <105588893+antongotech@users.noreply.github.com>
2022-12-13 13:52:23 +0200

    fixed

--- src/lib/useContractAddress.ts
+++ src/lib/useContractAddress.ts
@@ -1,9 +1,17 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { Address } from "ton";
+import { useEffect } from "react";
 
 function useContractAddress() {
+  const navigate = useNavigate();
   const { contractAddress } = useParams();
   let isAddressValid = validateAddress(contractAddress);
+
+  useEffect(() => {
+    if (isAddressValid) {
+      navigate(`/${Address.parse(contractAddress ?? "").toFriendly()}`, { replace: true });
+    }
+  }, [contractAddress]);
 
   return {
     contractAddress,
@@ -12,13 +20,10 @@ function useContractAddress() {
 }
 
 function validateAddress(contractAddress: string | undefined) {
-  const navigate = useNavigate();
   let isAddressValid = true;
 
   try {
-    if (contractAddress !== Address.parse(contractAddress ?? "").toFriendly()) {
-      navigate(`/${Address.parse(contractAddress ?? "").toFriendly()}`, { replace: true });
-    }
+    Address.parse(contractAddress ?? "").toFriendly();
   } catch (e) {
     isAddressValid = false;
   }
