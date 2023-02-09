fe082d9d01eaf1fdc83e663e8f53faf7d520c722
Shahar Yakir <shahar.yakir@gmail.com>
2023-02-09 11:07:41 +0200

    layout fixes (ellipsis, balance/data hash) (#156)

--- src/components/ContractBlock.tsx
+++ src/components/ContractBlock.tsx
@@ -46,6 +46,10 @@ export function ContractBlock() {
       subtitle: workchainForAddress(contractAddress || ""),
     });
     dataRows.push({
+      title: "Balance",
+      value: `${formatBalance.format(parseFloat(data.balance))} TON`,
+    });
+    dataRows.push({
       title: "Code Hash",
       value: displayCodeCellHash ?? "",
       showIcon: true,
@@ -63,10 +67,6 @@ export function ContractBlock() {
       },
       tooltip: true,
     });
-    dataRows.push({
-      title: "Balance",
-      value: `${formatBalance.format(parseFloat(data.balance))} TON`,
-    });
   }
 
   return (
@@ -75,7 +75,7 @@ export function ContractBlock() {
       icon={contractIcon}
       dataRows={dataRows}
       isLoading={isLoading}
-      isFlexibleWrapper={proofData && proofData.hasOnchainProof}
+      isFlexibleWrapper={!!proofData?.hasOnchainProof}
     />
   );
 }
