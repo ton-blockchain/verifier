77a93fce80bcb63ed50f4a1ae7dfd1c35038ca6d
Shahar Yakir <shahar.yakir@gmail.com>
2023-03-12 13:50:11 +0200

    admin page changes (#164)

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,21 +1,34 @@
+import { Box } from "@mui/material";
+import { TestnetBar } from "../TestnetBar";
 import { WalletConnect } from "../WalletConnect";
 import SourcesRegistry from "./SourcesRegistry";
 import { VerifierRegistry } from "./VerifierRegistry";
+import { FlexBoxRow } from "../Getters.styled";
+import { Footer } from "../Footer";
+import { useEffect } from "react";
+import { useWalletConnect } from "../../lib/useWalletConnect";
 
 export function Admin() {
+  const { restoreConnection } = useWalletConnect();
+
+  useEffect(() => {
+    restoreConnection();
+  }, []);
+
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
+      {window.isTestnet && <TestnetBar />}
+      <FlexBoxRow
+        sx={{
           gap: 30,
-          alignItems: "center",
+          px: 4,
         }}>
         <h1>Admin</h1>
         <WalletConnect />
-      </div>
+      </FlexBoxRow>
       <SourcesRegistry />
       <VerifierRegistry />
+      <Footer />
     </div>
   );
 }
