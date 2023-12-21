aad4907196ff99bd7e24795557ec2974875b5fbe
Anton Tsivarev <tsivarev.a@gmail.com>
2023-12-21 16:09:21 +0100

    Add 'base' property for vita config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/verifier/",
   build: {
     // sourcemap: true,
     target: ["es2020"],
